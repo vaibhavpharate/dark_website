8cb8c241e46eee29a6eac40f544cf398e14f4a8b
vaibhavpharate <62934219+vaibhavpharate@users.noreply.github.com>
2023-11-28 17:44:00 +0530

    Update views.py

--- website/views.py
+++ website/views.py
@@ -20,6 +20,7 @@ from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.models import Permission
 from django.contrib import messages
 from django.contrib.auth.models import Group
+from django.conf import settings as django_settings
 
 # py data plots
 from plotly import express as px
@@ -41,7 +42,8 @@ from .forms import ClientsForm
 User = get_user_model()
 token = settings.MAPBOX_TOKEN
 px.set_mapbox_access_token(token)
-
+data_file_path = os.path.join(django_settings.STATICFILES_DIRS[0],'data')
+    
 date_format = '%Y-%m-%d %H:%M:%S'
 # Create your views here.
 
@@ -143,8 +145,9 @@ def admin_logout(request):
 
 def client_logout(request):
     username = request.user.username
+    
     if check_data_store(request):
-        os.remove(f'static/data/{username}.csv')
+        os.remove(os.path.join(data_file_path,f'{username}.csv'))
     logout(request)
     messages.warning(request, "You have successfully logged out.")
     return redirect('client_login')
@@ -189,7 +192,7 @@ def get_data_store(username):
     date = datetime.strftime(date,'%Y-%m-%d 00:00:00')
     sites = get_site_client(client_name=username, type='Solar')
     sites = tuple(sites)
-
+    print(data_file_path)
     query = f"""SELECT vda.site_name,
                                 vda.timestamp,
                                vda.wind_speed_10m_mps AS wind_speed_forecast,
@@ -216,18 +219,19 @@ def get_data_store(username):
                         AND conf.type='Solar'  ORDER BY vda.timestamp desc"""
 
     df = get_sql_data(query)
-    df.to_csv(f'static/data/{username}.csv',index=False)
+    df.to_csv(os.path.join(data_file_path,f'{username}.csv'),index=False)
     return f"Data Store created for {username} for date {date}"
 
 def check_data_store(request):
     username = request.user.username
-    return os.path.exists(f"static/data/{username}.csv")
+    return os.path.exists(os.path.join(data_file_path,f'{username}.csv'))
 
 @login_required(login_url='client_login')
 def homepage(request):
     context = {}
     if not check_data_store(request):
         print(get_data_store(request.user.username))
+    print(check_data_store(request))
     return render(request, template_name='website/homepage.html', context=context)
 
 
@@ -337,7 +341,7 @@ def get_homepage_data(request):
 
             ci_index = 0.1
             # df = get_sql_data(query)
-            df = pd.read_csv(f'static/data/{client_name}.csv')
+            df = pd.read_csv(os.path.join(data_file_path,f'{client_name}.csv'))
             df = df.rename({'site_client_name':'client_name'},axis='columns')
             # print(df.columns)
             df = df.groupby(['timestamp', 'site_name', 'client_name']).aggregate(
@@ -492,7 +496,7 @@ def get_forecast_table(request):
                         AND conf.type='Solar'  ORDER BY vda.timestamp desc LIMIT 10000;"""
 
         # df_4 = get_sql_data(query)
-        df_4 = pd.read_csv(f'static/data/{username}.csv')
+        df_4 = pd.read_csv(os.path.join(data_file_path,f'{username}.csv'))
         ci_index = 0.1
         df_4 = df_4.loc[df_4['site_status']=='Active',:]
         df_4['forecast_cloud_type'] = df_4['forecast_cloud_type'].fillna('No Cloud')  ## Old Query
@@ -546,7 +550,7 @@ def get_fw_data(request):
             FROM forecast.v_db_api vda JOIN configs.site_config conf ON vda.site_name = conf.site_name 
             LEFT JOIN site_actual.site_actual sa on (vda.timestamp,vda.site_name) = (sa.timestamp,sa.site_name) 
             WHERE conf.site_name = '{site_name}' AND vda.ci_data IS NOT NULL  AND vda.timestamp > '{start_date}' AND vda.timestamp <= '{end_date}'  ORDER BY timestamp DESC"""
-        df = pd.read_csv(f'static/data/{username}.csv')
+        df = pd.read_csv(os.path.join(data_file_path,f'{username}.csv'))
         df['timestamp'] = pd.to_datetime(df['timestamp'],format=date_format)
         min_date = df.loc[:,'timestamp'].min()
         max_date = df.loc[:,'timestamp'].max()
@@ -697,7 +701,7 @@ def get_warnings_data(request):
         group = request.GET['username']
         ci_index = 0.1
         # Get the Data Frame
-        df = pd.read_csv(f'static/data/{username}.csv')
+        df = pd.read_csv(os.path.join(data_file_path,f'{username}.csv'))
         
         now_timestamp = datetime.now()
         three_hours_plus = now_timestamp + timedelta(hours=3)
@@ -816,7 +820,7 @@ def update_on_site_change(request):
         if group == "Admin":
             query = ""
         else:
-            df = pd.read_csv(f'static/data/{client}.csv')
+            df = pd.read_csv(os.path.join(data_file_path,f'{client}.csv'))
             df = df.rename({'site_client_name':'client_name'},axis='columns')
         ci_index = 0.1
         # df = get_sql_data(query)
